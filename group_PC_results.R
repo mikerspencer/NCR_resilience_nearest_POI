278c4ff74f0f3f782b3bd10f54ed7f11690bdb16
mikerspencer <spencer.mike.r@gmail.com>
2019-09-04 12:38:12 +0100

    tidied repo prior to making public

--- group_PC_results.R
+++
@@ -1,7 +0,0 @@
-x@data %>% 
-   as_tibble() %>% 
-   group_by(datazone) %>% 
-   summarise(dist = median(dist_km, na.rm=T)) %>% 
-   mutate(dist = replace_na(dist, -999)) %>% 
-   drop_na() %>% 
-   write_csv("Downloads/temp.csv")
